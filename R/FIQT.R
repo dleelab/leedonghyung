f9f26c7110bc1d7be0ad80f69422f3616e0054d4
dleelab <dustorm@gmail.com>
2018-06-21 17:48:12 -0400

    FIQT added

---
+++ R/FIQT.R
@@ -0,0 +1,19 @@
+#' A function for FDR Inverse Quantile Transformation (FIQT)
+#'
+#' This function computes the FIQT (winner's curse) adjusted assocation Z-scores
+#'
+#' @param z a vector of association Z-scores
+#' @param min.p minimum p-value admitted (to avoid zero p-values/adjusted p-values which give troubles with inverse cdf)
+#' very large Z-scores corresponding to min.p (i.e., z > 37) are not adjusted, as their bias is essentially zero
+#'
+#' @return mu.z a vector of FIQT (winner's curse) adjusted association Z-scores
+#'
+
+FIQT <- function(z=z, min.p=10^-300){
+  pvals<-2*pnorm(abs(z),low=F)
+  pvals[pvals<min.p]<- min.p
+  adj.pvals<-p.adjust(pvals,method="fdr")
+  mu.z<-sign(z)*qnorm(adj.pvals/2,low=F)
+  mu.z[abs(z)>qnorm(min.p/2,low=F)]<-z[abs(z)>qnorm(min.p/2,low=F)]
+  return(mu.z)
+}
